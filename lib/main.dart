48fed4673061f09a9b7d6ebd89aefdac30b09728
Sharon Atim <venusatim@gmail.com>
2023-03-28 13:14:19 +0100

    getdevicelocale method added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -6,16 +8,46 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+String getDeviceLocale() {
+  return window.locale.languageCode;
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    getDeviceLocale();
+    debugPrint(window.locale.languageCode);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // TODO: How to pass in the localized string into the application title
+      // onGenerateTitle: (context) => DemoLocalizations.of(context).title,
+
       title: 'flutter Internationalisation Demo',
       // LocalizationsDelegates defines a list of delegates that Flutter will
       // use to look up localised strings and other resources for the app.
+
+      // Once the codegen generates the gen_l10 folder, The AppLocalizations class also provides auto-generated
+      // localizationsDelegates and supportedLocales lists. You can use these instead of providing them manually.
+      // you can use the following instead of the localizationsDelegates list property:
+
+      // Uncomment below *
+      // localizationsDelegates: AppLocalizations.localizationsDelegates,
+      // supportedLocales: AppLocalizations.supportedLocales,
+
       localizationsDelegates: const [
+        AppLocalizations
+            .delegate, // This line is added after codegen generates the gen_l10n folder.
         GlobalMaterialLocalizations
             .delegate, // Provides localized strings and other values for the Material Components library.
         GlobalWidgetsLocalizations
@@ -49,38 +81,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        // The AppBar title will now update its message according to the target
+        // platform. Switching the device's locale between English, Spanish and
+        // French locales should cause this text to update.
+        title: Text(AppLocalizations.of(context)!.helloWorld),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              AppLocalizations.of(context)!.hello('Sharon'),
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
